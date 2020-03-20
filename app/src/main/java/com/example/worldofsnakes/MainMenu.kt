45306047fd602fd483a0731586daed513545b71e
JenfX <55789345+JenfX@users.noreply.github.com>
2020-03-20 19:43:06 +0700

    test2

--- app/src/main/java/com/example/worldofsnakes/MainMenu.kt
+++ app/src/main/java/com/example/worldofsnakes/MainMenu.kt
@@ -2,12 +2,16 @@ package com.example.worldofsnakes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button;
 
-class MainActivity : AppCompatActivity() {
+class MainMenu : AppCompatActivity() {
+
+    //private Button singleplayer;
 
     @Override
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_menu)
+
     }
 }
